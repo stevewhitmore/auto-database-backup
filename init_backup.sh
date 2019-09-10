c0d3af27ae9e75f9bc4dceb0153beca3933de3e2
Steve Whitmore <steve@swhitmore.com>
2019-09-09 23:59:22 -0500

    Added missing destination

--- init_backup.sh
+++ init_backup.sh
@@ -4,6 +4,7 @@
 
 host_at_username=$(awk -F= '/host_at_username/ { print $2 }' config.txt)
 remote_dir=$(awk -F= '/remote_dir/ { print $2 }' config.txt)
+local_dir=$(awk -F= '/local_dir/ { print $2 }' config.txt)
 
 sshpass -p 'some_superfly_password' scp backup.sh "$host_at_username":"$remote_dir"
 
@@ -12,4 +13,4 @@ source backup.sh
 updated_filename=${db_name%.*}-$(date +%Y%m%d).sql
 
 sleep 30s
-sshpass -p 'some_superfly_password' scp "$host_at_username":"$remote_dir"/"${updated_filename}".tar.gz
+sshpass -p 'some_superfly_password' scp "$host_at_username":"$remote_dir"/"${updated_filename}".tar.gz "$local_dir"
